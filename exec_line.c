8e94bc88bf1490e09a9ad3e53d33d495188090bd
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-05-16 14:38:25 +0300

    Update exec_line.c

--- exec_line.c
+++ exec_line.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * allocate_buffer - allocates memory
+ * sh_buffer - allocates memory
  * @buf_size: memory size
  * Return: allocated memory address
  */
 
-char *allocate_buffer(size_t buf_size)
+char *sh_buffer(size_t buf_size)
 {
 	char *buf = (char *) malloc(buf_size);
 
@@ -19,14 +19,14 @@ char *allocate_buffer(size_t buf_size)
 }
 
 /**
- * expand_buffer - increases size of allocated memory
+ * sh_exbuffer - increases size of allocated memory
  * @buf_size: memory size
  * @buf: pointer to allocated memory
  * Return: newly allocated memory address
  */
-char *expand_buffer(char *buf, size_t buf_size)
+char *sh_exbuffer(char *buf, size_t buf_size)
 {
-	char *newBuf = (char *) my_realloc(buf, buf_size);
+	char *newBuf = (char *) sh_realloc(buf, buf_size);
 
 	buf_size *= 2;
 	if (newBuf == NULL)
@@ -39,23 +39,23 @@ char *expand_buffer(char *buf, size_t buf_size)
 }
 
 /**
- * is_empty_line - checks if empty line is passed
+ * sh_checkempty - checks if empty line is passed
  * @len: length of string
  * @buf: pointer to allocated memory
  * Return: newly allocated memory address
  */
-int is_empty_line(ssize_t len, char *buf)
+int sh_checkempty(ssize_t len, char *buf)
 {
 	return (len == 0 || (len == 1 && buf[0] == '\n'));
 }
 
 /**
- * handle_empty_line - handles empty line passed
+ * sh_handleempty - handles empty line passed
  * @lineptr: double pointer
  * @buf: pointer to allocated memory
  * Return: -1
  */
-int handle_empty_line(char *buf, char **lineptr)
+int sh_handleempty(char *buf, char **lineptr)
 {
 	free(buf);
 	*lineptr = NULL;
@@ -63,11 +63,11 @@ int handle_empty_line(char *buf, char **lineptr)
 }
 
 /**
- * terminate_buffer - terminates the buffer
+ * sh_terminator - terminates the buffer
  * @len: length of string
  * @buf: pointer to allocated memory
  */
-void terminate_buffer(ssize_t len, char *buf)
+void sh_terminator(ssize_t len, char *buf)
 {
 	buf[len] = '\0';
 }
