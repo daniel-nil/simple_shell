d9ef6b3802642f16209410cbc4f56a61ab21aede
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-05-16 14:45:34 +0300

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -8,14 +8,14 @@
 
 
 /**
- * my_getline - custom version of getline()
+ * sh_getline - custom version of getline()
  * @lineptr: buffer for storing strings
  * @n: size of buffer
  * @fd: source to read from
  *
  * Return: -1 or number of char's read
  */
-ssize_t my_getline(char **lineptr, size_t *n, int fd)
+ssize_t sh_getline(char **lineptr, size_t *n, int fd)
 {
 	ssize_t len = 0;
 	size_t buf_size = *n;
@@ -30,7 +30,7 @@ ssize_t my_getline(char **lineptr, size_t *n, int fd)
 	if (buf == NULL)
 	{
 		buf_size = INIT_BUF_SIZE;
-		buf = allocate_buffer(buf_size);
+		buf = sh_buffer(buf_size);
 		if (buf == NULL)
 		return (-1);
 		*lineptr = buf;
@@ -39,7 +39,7 @@ ssize_t my_getline(char **lineptr, size_t *n, int fd)
 	{
 		if (len + 1 >= (ssize_t)buf_size)
 		{
-			char *newBuf = expand_buffer(buf, buf_size);
+			char *newBuf = sh_exbuffer(buf, buf_size);
 
 			if (newBuf == NULL)
 			return (-1);
@@ -51,8 +51,8 @@ ssize_t my_getline(char **lineptr, size_t *n, int fd)
 		if (c == '\n')
 			break;
 	}
-	if (is_empty_line(len, buf))
-		return (handle_empty_line(buf, lineptr));
-	terminate_buffer(len, buf);
+	if (sh_checkempty(len, buf))
+		return (sh_handleempty(buf, lineptr));
+	sh_terminator(len, buf);
 	return (len);
 }
