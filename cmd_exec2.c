b4cc97d5fb11ac865cf311cfd9a5159adfd02e15
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-05-16 08:54:34 +0300

    Update cmd_exec2.c

--- cmd_exec2.c
+++ cmd_exec2.c
@@ -13,11 +13,11 @@ void handle_semicolon(char *input)
 
 	delim_tokenize(input, commands, MAX_NUM_TOKENS, ";");
 
-	/* Execute each command in sequence */
+
 	for (j = 0; commands[j] != NULL; j++)
 	{
 		delim_tokenize(commands[j], args, MAX_NUM_TOKENS, " \t\n");
-		if (args[0] != NULL)/*check if user passes arguments*/
+		if (args[0] != NULL)
 		{
 			pid = fork();
 			if (pid == 0)
@@ -52,12 +52,12 @@ void handle_semicolon(char *input)
 int delim_tokenize(char *input, char **tokens, int max_tokens, char *delim)
 {
 	int num_tokens = 0;
-	char *token = strtok(input, delim); /* get first token/string */
+	char *token = strtok(input, delim);
 
 	while (token != NULL && num_tokens < max_tokens)
 	{
-		tokens[num_tokens] = token; /* populate "tokens" array with strings */
-		num_tokens++; /* move to next index in "tokens" array */
+		tokens[num_tokens] = token;
+		num_tokens++;
 		token = strtok(NULL, delim);
 	}
 	tokens[num_tokens] = NULL;
