b03400fac6c63c631b171848676bfe197ddac982
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-05-17 12:59:49 +0300

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -9,50 +9,50 @@
 
 /**
  * sh_getline - custom version of getline()
- * @lineptr: buffer for storing strings
- * @n: size of buffer
+ * @lptr: buffer for storing strings
+ * @x: size of buffer
  * @fd: source to read from
  *
  * Return: -1 or number of char's read
  */
-ssize_t sh_getline(char **lineptr, size_t *n, int fd)
+ssize_t sh_getline(char **lptr, size_t *x, int fd)
 {
 	ssize_t len = 0;
-	size_t buf_size = *n;
-	char *buf = *lineptr;
+	size_t b_size = *x;
+	char *b = *lptr;
 	char c = 0;
 
-	if (lineptr == NULL || n == NULL)
+	if (lptr == NULL || x == NULL)
 	{
 		errno = EINVAL;
 		return (-1);
 	}
-	if (buf == NULL)
+	if (b == NULL)
 	{
-		buf_size = BUFFERSIZE;
-		buf = sh_buffer(buf_size);
-		if (buf == NULL)
+		b_size = BUFFERSIZE;
+		b = sh_buffer(b_size);
+		if (b == NULL)
 		return (-1);
-		*lineptr = buf;
+		*lptr = b;
 	}
 	while (read(fd, &c, 1) > 0)
 	{
-		if (len + 1 >= (ssize_t)buf_size)
+		if (len + 1 >= (ssize_t)b_size)
 		{
-			char *newBuf = sh_exbuffer(buf, buf_size);
+			char *new_b = sh_exbuffer(b, b_size);
 
-			if (newBuf == NULL)
+			if (new_b == NULL)
 			return (-1);
-			buf = newBuf;
-			*lineptr = buf;
-			buf_size *= 2;
+			b = new_b;
+			*lptr = b;
+			b_size *= 2;
 		}
-		buf[len++] = c;
+		b[len++] = c;
 		if (c == '\n')
 			break;
 	}
-	if (sh_checkempty(len, buf))
-		return (sh_handleempty(buf, lineptr));
-	sh_terminator(len, buf);
+	if (sh_checkempty(len, b))
+		return (sh_handleempty(b, lptr));
+	sh_terminator(len, b);
 	return (len);
 }
